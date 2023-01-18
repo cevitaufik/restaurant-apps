99d081ed39b98ecd55a45aff431bc7a1b30f2364
Cevi Taufik <cevitaufik@gmail.com>
2023-01-18 23:00:09 +0700

    perbaikan tampilan mobile

--- submission_1/src/scripts/index.js
+++ submission_1/src/scripts/index.js
@@ -3,3 +3,11 @@ import '../styles/css/main.css'
 import restaurants from '../DATA.json'
 
 console.log(restaurants)
+
+document
+  .getElementById('menu')
+  .addEventListener('click', e => {
+    const navbarContainer = document.querySelector('.navbar .navbar-container')
+    navbarContainer.classList.toggle('show')
+    navbarContainer.classList.toggle('hide')
+  })
