c9e9697d24d5bbcfdaa18bbae5480415f1b4e38a
Cevi Taufik <cevitaufik@gmail.com>
2023-01-20 00:14:29 +0700

    finalisasi

--- submission_1/src/scripts/index.js
+++ submission_1/src/scripts/index.js
@@ -1,8 +1,8 @@
 import 'regenerator-runtime' /* for async await transpile */
 import '../styles/css/main.css'
-import restaurants from '../DATA.json'
+import data from '../DATA.json'
 
-console.log(restaurants)
+console.log(data.restaurants)
 
 document
   .getElementById('menu')
@@ -11,3 +11,23 @@ document
     navbarContainer.classList.toggle('show')
     navbarContainer.classList.toggle('hide')
   })
+
+const baseArticle = document.querySelector('.content article')
+
+function cloneElement (data) {
+  const newArticle = baseArticle.cloneNode(true)
+
+  newArticle.setAttribute('id', data.id)
+  newArticle.querySelector('img').setAttribute('src', data.pictureId)
+  newArticle.querySelector('img').setAttribute('alt', data.name)
+  newArticle.querySelector('h3').innerText = data.name
+  newArticle.querySelector('.rating #number').innerText = data.rating
+  newArticle.querySelector('.location #city').innerText = data.city
+  newArticle.querySelector('#description').innerText = data.description
+
+  baseArticle.parentElement.insertAdjacentElement('afterbegin', newArticle)
+}
+
+data.restaurants.forEach(data => cloneElement(data))
+
+baseArticle.remove()
